2fe6a1ae6e14abce639a2dd99edd3bdbc0662117
Dianne Saba <dnsaba19@gmail.com>
2017-12-13 17:20:20 -0800

    Add project files.

---
+++ WebApiApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApiApp.Startup))]
+namespace WebApiApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ WebApiApp/app/public/app.js
@@ -0,0 +1,28 @@
+﻿angular.module('routerApp', ['ui.router', 'routerApp.routes']);
+
+var myApp = angular.module("routerApp" + '.routes', []); 
+myApp.config(function ($stateProvider, $locationProvider, $urlRouterProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false,
+    });
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            name: 'home',
+            url: '/home',
+            templateUrl: '/app/public/partialHome.html'
+        })
+
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            name: 'about',
+            url: '/about',
+            templateUrl: '/app/public/partialAbout.html'
+        });
+
+});
+
