501a992d19e1be9ec03fda2d4ff91a63c86dd3e1
Dianne Saba <dnsaba19@gmail.com>
2017-12-19 14:32:27 -0800

    Web scraping works

---
+++ WebApiApp/Controllers/api/UrlDataController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiApp.Models.Domain;
+using WebApiApp.Responses;
+using WebApiApp.Services;
+
+namespace WebApiApp.Controllers.api
+{
+    [RoutePrefix("api/urlData")]
+    public class UrlDataController : ApiController
+    {
+        UrlDataService svc = new UrlDataService();
+
+        [Route("get2"), HttpPost]
+        [AllowAnonymous]
+        public HttpResponseMessage Get2(Urls model)
+        {
+            try
+            {
+                ItemResponse<UrlData> resp = new ItemResponse<UrlData>();
+                resp.Item = svc.GetCards(model.Url);
+                return Request.CreateResponse(HttpStatusCode.OK, resp);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+    }
+}
---
+++ WebApiApp/Models/Domain/Urls.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiApp.Models.Domain
+{
+    public class Urls
+    {
+        public string Url { get; set; }
+    }
+}
