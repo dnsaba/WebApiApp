cccb356d1678c9202bc6ba9ae0258de267809500
Dianne Saba <dnsaba19@gmail.com>
2018-02-05 18:31:03 -0800

    Reorganized file structure, starting changes to make this an app to teach kids how to code

---
+++ WebApiApp.Models/Domain/Card.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiApp.Models.Domain
+{
+    public class Card
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int AttackLevel { get; set; }
+        public int DefenseLevel { get; set; }
+    }
+}
---
+++ WebApiApp.Web/app/public/modules/profile/profile.service.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("publicApp")
+        .factory("profileService", ProfileService);
+
+    ProfileService.$inject = ["$http", "$q"];
+
+    function ProfileService($http, $q) {
+        return {
+            uploadFile: _uploadFile
+        };
+
+        function _uploadFile(file) {
+            return $http.post("/api/profile/fileUpload", file, { withCredentials: true })
+                .then(success).catch(error);
+        }
+
+        function success(res) {
+            return (res);
+        }
+
+        function error(err) {
+            return (err);
+        }
+    }
+})();
