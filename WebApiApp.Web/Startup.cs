0ca454ce488ee210db8a1350e4c0c88bab020f65
Dianne Saba <dnsaba19@gmail.com>
2018-02-11 16:31:06 -0800

    Working on login/authentication

--- WebApiApp.Web/Startup.cs
+++ WebApiApp.Web/Startup.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartupAttribute(typeof(WebApiApp.Startup))]
-namespace WebApiApp
-{
-    public partial class Startup
-    {
-        public void Configuration(IAppBuilder app)
-        {
-            ConfigureAuth(app);
-        }
-    }
-}
+//[assembly: OwinStartupAttribute(typeof(WebApiApp.Startup))]
+//namespace WebApiApp
+//{
+//    public partial class Startup
+//    {
+//        public void Configuration(IAppBuilder app)
+//        {
+//            ConfigureAuth(app);
+//        }
+//    }
+//}
